b2cdddf05f24d338a5c0a5c5b6c6e81a8ad845f6
TomerGelberg <104442181+TomerGelberg@users.noreply.github.com>
2022-04-27 12:51:23 +0300

    Add files via upload

--- my_version.py
+++ my_version.py
@@ -1,5 +1,12 @@
 import sys
 def my_version():
-    return (sys.version)  
-my_version
+    versiom_as_regex=sys.version
+    counter=0
+    string=''
+    while (versiom_as_regex[counter] != ' '):
+        string=string + versiom_as_regex[counter]
+        counter=counter+1
+    return (string)  
+my_version()
+
 
